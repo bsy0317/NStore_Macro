72979777cd512b67526490682af2fb890a91b4e0
Bae Seoyeon <talk@kakao.one>
2022-01-24 14:59:28 +0900

    Selenium 이용방법으로 수정

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import hmac
 import unicodedata        
 import requests
 import rich
+import selenium
+import chromedriver_autoinstaller
 
 from rich import print
 from rich import box
@@ -16,39 +18,57 @@ from rich.columns import Columns
 from rich.table import Table
 console = Console()
 
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
 def main():
-    URL = "https://smartstore.naver.com/ooooofish/products/5649019336"
-    request_headers = {'User-Agent' : ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/605.1.15\
-    (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/605.1.15'),
-    'Referer': 'https://cr.shopping.naver.com/adcr.nhn?x=5xgk0o%2FxXFpmUSEqnZiMAf%2F%2F%2Fw%3D%3Ds%2BCWfv631H6tObR52AEQQDt6jWWebTbkeJHb44kZ52rMGpuC4VbNVyxN%2BtJ0rLKH0OCkyosZuN3LkYlnxTdBJ7ZjQgYbrZL9aR%2FDkx82JiD1IMvbPuiP%2FjrRXSeOxXFWL0X90L1ljkAEVSjmStAe999kzUcfthzKjbftAcC5CTHPvbFbCHc5MwHtep5oytzJblFkJv7%2FGg8vgWY87RMjS3jlrq1uM7bPAMzmOluPP7EtD4P%2Bj2CVvS6QkrpszyaIo6sGkobrHOLjdQ9PfNparAPguzbdcNHkPsAELGL7l%2F50fi1cp%2B8QtYpmHeRMAbPQhE4Dx5kATpPTRMEyS1hVWyMmscUZuB%2FB3GA4tAdwd4kJn0E%2B7lsL8Xc%2FJzL4VwihdN00CswaTs22ObTnm4j0ulljCt58qbD%2BRfesCX7ikynyi4%2Fr0lz1oSk9AW8qzFsUvpOr9sPYwajBkeE07IGGkJ5SCPsmGKllbt0kl8XIxCXSaW9%2B2TdBqLfvUrowSfyGYWLZaZga65DYcSsOcEod%2B70xsS3tSAApU8%2F60dhmqvG4ZIonLZYIZnUXNxdOrUDQzs586Lq5uuwnPH0HpXU3Joto7MmLwgspaDMrKjkxnlIbAkjdKbZ0ldlv%2FmgNh%2B4NIpq0hLmtxqBcD3Jax19oBLL8KMQ5XVUesE3eI%2FTjQDVrWDLsEKMWwV8kWkvfATCkeB3JWD2w80rMEju2mDPG8o%2BvM3s8AGAYQEMSVntmJzwBtsTjGrOlp0XPMI1xP90YRgRA2fGaKe9voP9GEohFgtnySN8h8%2FaZOQGyCkSmLkMZMgRMcs3KzajSOCbGpFQmo8bqU4650%2BGkhpOfxP7ZIIYu5mW8mvZFd3nEYzo3VOiSQ%2BMQQRSuVrpKm8ndhhZzDQdAiJdwXirBRdnaxfvrBbw%3D%3D&nvMid=83193516581&catId=50004694'}
+    URL = "https://smartstore.naver.com/ooooofish"
     count = 0
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36")
+    options.add_argument("app-version=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36")
+    
     while True:
         proxy_server = get_proxy().split("/")
-        proxies = {"TYPE":"HOST:PORT"}
-        if proxy_server[0] == "HTTP":
-            proxies = {"http": proxy_server[1]}
-        elif proxy_server[0] == "HTTPS":
-            proxies = {"https": proxy_server[1]}
+        options.add_argument(f"--proxy-server={proxy_server[0].lower()}://{proxy_server[1]}")
         console.log(f"[{proxy_server[0]}] Proxy 할당 완료 ({proxy_server[1]})")
         
         try:
-            resp = requests.get(URL, headers=request_headers, proxies=proxies, timeout=10)
-            console.log(f"[@] Request Succeddful. StatusCode({resp.status_code})")
-            txt= resp.text
+            driver = webdriver.Chrome(executable_path='chromedriver',options=options)
+            driver.get(URL)
+            element = WebDriverWait(driver,30).until(EC.presence_of_element_located((By.ID, "content")))
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            console.log(f"[@] Request Succeddful.")
         except Exception as e:
             console.log("[!] Proxy Conn Failure.")
             console.log(e)
-        time.sleep(2)
         
+        driver.delete_all_cookies()
+        driver.quit()
     return 0
     
 def get_proxy():
-    response = requests.get("https://www.proxyscan.io/api/proxy?level=elite&Type=http,https&ping=200&last_check=3600")
+    response = requests.get("https://www.proxyscan.io/api/proxy?Type=http,https&ping=100&last_check=1800")
     res_json = response.json()
     return f"{res_json[0]['Type'][0]}/{res_json[0]['Ip']}:{str(res_json[0]['Port'])}"
 
 if __name__ == "__main__":
     try:
+        chromedriver_autoinstaller.install() 
         main()
     except Exception as ex:
         if(ex == KeyboardInterrupt):
